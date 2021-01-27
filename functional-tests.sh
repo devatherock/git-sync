5fa185ab674bebbe651d59bb461f4c194499fc50
devatherock <devatherock@gmail.com>
2021-01-27 02:26:49 -0600

    chore: Fixed another Codacy violation

--- functional-tests.sh
+++ functional-tests.sh
@@ -3,9 +3,9 @@
 cp -r . ../git-sync-test
 cd ../git-sync-test
 export CURRENT_BRANCH=$(git symbolic-ref --short HEAD)
-git remote set-url origin https://git-sync-token:$GIT_TOKEN@github.com/devatherock/git-sync.git
+git remote set-url origin "https://git-sync-token:$GIT_TOKEN@github.com/devatherock/git-sync.git"
 ./gradlew test --tests '*SyncGitReposDockerSpec*' -x jacocoTestCoverageVerification
 exit_code=$?
 cd ../git-sync
 rm -rf ../git-sync-test
-exit $?
+exit $exit_code
